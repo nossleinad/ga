fbc8c7c47885e890e808257b6cb50252be92ff2c
maxim <maximilian.danielsson@gmail.com>
2022-10-01 00:58:30 +0100

    debugging reproduce and introducing mutations in reproduction

--- ga.py
+++ ga.py
@@ -1,10 +1,13 @@
 import pygame
 import time
 import random
-from pygame.math import Vector3
-from pygame.math import Vector2
 import math
 import statistics
+from pygame.math import Vector3
+from pygame.math import Vector2
+from collections import deque
+
+
 
 pygame.init()
 
@@ -42,60 +45,77 @@ def show_fps(delta_time, text_color=(0, 255, 0), outline_color=(0, 0, 0)):
 
 angle = math.pi / 2
 
-threshold_min = 0.02
-threshold_max = 0.98
-
 reproduce_counter = 0
 death_counter = 0
-average = []
+white_counter = 0
+black_counter = 0
+
+average = deque(maxlen=200)
 
 
 class PepperMoth:
     def __init__(self, radius, pos, color=None):
+        global white_counter
+        global black_counter
         self.radius = radius
         self.pos = pos
-        self.color = random.choice((v_white, v_black))
+        self.color = color if color is not None else random.choice((v_white, v_black))
+        white_counter += bool(self.color)
+        black_counter += not bool(self.color)
 
     def __str__(self):
         return str(self.__dict__)
 
     def reproduce(self):
-        return PepperMoth(self.radius, [random.randint(150, 1050), random.randint(150, 750)], self.color)
+        m_prob = 0.02
+        if m_prob < random.uniform(0, 1):
+            color = self.color
+        else:
+            color = Vector3(255, 255, 255) - self.color
+        return PepperMoth(self.radius, [random.randint(150, 1050), random.randint(150, 750)], color)
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, self.pos, self.radius)
 
 
-def make_peppermoths(n, seed=30):
+def make_peppermoths(n, seed=1000):
     random.seed(seed)
     return [PepperMoth(10, [random.randint(150, 1050), random.randint(150, 750)]) for _ in range(n)]
 
 
-peppers = make_peppermoths(100)
+peppers = make_peppermoths(1000)
 
 
-def reproduce(r_prob: float = 0.275) -> None:
+def reproduce(r_prob: float = 0.3) -> None:
     global peppers
     global reproduce_counter
     for i in range(len(peppers)):
         if r_prob >= random.uniform(0, 1):
-            peppers.insert(i, peppers[i].reproduce())
+            peppers.append(peppers[i].reproduce())
             reproduce_counter += 1
 
 
-def die():
+def die(alpha=0.01): # alpha är amplituden för hur mycket d_prob svänger mellan det stabila d_prob = r_prob / (1 + r_prob)
     global peppers
     global death_counter
     global average
+    global white_counter
+    global black_counter
     i = 0
     while i < len(peppers):
-        d_prob = 0.25 + abs(peppers[i].color[0] / 255 - sin_angle) / 20
+        d_prob = 0.3 / 1.3 - alpha + abs(peppers[i].color[0] / 255 - sin_angle) * 2 * alpha
         average.append(d_prob)
         if d_prob >= random.uniform(0, 1):
+            white_counter -= bool(peppers[i].color)
+            black_counter -= not bool(peppers[i].color)
             peppers.pop(i)
             death_counter += 1
             continue
         i += 1
+
+
+# man kan inte ha samma prob för die och reproduce. Med d_prob=r_prob=0.5 får man genomsnittligt en sekvens: * 1.5 / 2 * 1.5 / 2
+# alltså minskar i genomsnitt populationen per uppdatering med faktor 3/4. För att inte få en dynamisk population kan man sätta d_prob = r_prob / (1 + r_prob)
 
 
 while run:
@@ -123,15 +143,21 @@ while run:
     angle += 0.1 * dt
     display.fill(background_color)
 
+
     for p in peppers:
         p.draw(display)
 
     reproduce()
     die()
+
+
     display.blit(font.render(f'Reproduce counter: {reproduce_counter}', True, v_red), (130, 0))
     display.blit(font.render(f'Death counter: {death_counter}', True, v_red), (600, 0))
     display.blit(font.render(f'Average d_prob: {statistics.mean(average):.2%}', True, v_red), (600, 40))
     display.blit(font.render(f'Population size: {len(peppers)}', True, v_red), (130, 40))
+    display.blit(font.render(f'White population size: {white_counter}', True, v_red), (130, 80))
+    display.blit(font.render(f'Black population size: {black_counter}', True, v_red), (600, 80))
+
     show_fps(dt)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(10)
