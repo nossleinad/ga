a87ca2ed44605472e7a0ad1b1c3cd79615f5f98f
maxim <maximilian.danielsson@gmail.com>
2022-11-17 21:32:22 +0100

    introducing diploid reproduction

--- ga.py
+++ ga.py
@@ -1,16 +1,17 @@
-import pygame
-import time
-import random
 import math
+import random
 import statistics
-from pygame.math import Vector3
-from pygame.math import Vector2
+import time
 from collections import deque
+
+import pygame
+from pygame.math import Vector2
+from pygame.math import Vector3
 
 pygame.init()
 
 WINDOW_RESOLUTION = Vector2(1200, 900)
-margin = 150 # avstånd mellan området med moths och window
+margin = 150  # avstånd mellan området med moths och window
 display = pygame.display.set_mode(WINDOW_RESOLUTION, pygame.RESIZABLE)
 pygame.display.set_caption(__file__.split("\\")[-1])
 
@@ -44,8 +45,6 @@ def show_fps(delta_time, text_color=(0, 255, 0), outline_color=(0, 0, 0)):
 
 angle = math.pi / 2
 
-reproduce_counter = 0
-death_counter = 0
 white_counter = 0
 black_counter = 0
 genotypes = ["BB", "Bw", "wB", "ww"]
@@ -68,39 +67,37 @@ class PepperMoth:
     def __str__(self):
         return str(self.__dict__)
 
-    def reproduce(self):
+    def reproduce(self, other):
         # mutations
         m_prob = 0.02
         if m_prob < random.uniform(0, 1):
-            genotype = self.genotype
+            genotype = random.choice(self.genotype) + random.choice(other.genotype)
         else:
             genotype = random.choice(genotypes)
-        return PepperMoth(self.radius, [random.randint(150, WINDOW_RESOLUTION[0] - margin),
-                                        random.randint(150, WINDOW_RESOLUTION[1] - margin)], genotype)
+        return PepperMoth(self.radius, Vector2(random.randint(margin, int(WINDOW_RESOLUTION[0] - margin)),
+                                               random.randint(margin, int(WINDOW_RESOLUTION[1] - margin))), genotype)
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, self.pos, self.radius)
 
 
-def make_peppermoths(n, seed=10):
+def make_peppermoths(n, seed=100):
     random.seed(seed)
-    return [PepperMoth(5, [random.randint(150, WINDOW_RESOLUTION[0] - margin),
-                           random.randint(150, WINDOW_RESOLUTION[1] - margin)]) for _ in range(n)]
+    return [PepperMoth(5, Vector2(random.randint(margin, int(WINDOW_RESOLUTION[0] - margin)),
+                                  random.randint(margin, int(WINDOW_RESOLUTION[1] - margin)))) for _ in range(n)]
 
 
 peppers = make_peppermoths(100)
 r_prob = 0.3
 
+
 def reproduce(r_prob) -> None:
-    global reproduce_counter
     for i in range(len(peppers)):
         if r_prob >= random.uniform(0, 1):
-            peppers.append(peppers[i].reproduce())
-            reproduce_counter += 1
+            peppers.append(peppers[i].reproduce(random.choice(peppers)))
 
 
-def die(alpha=0.05):  # alpha är amplituden för hur mycket d_prob svänger mellan det stabila d_prob = r_prob / (1 + r_prob)
-    global death_counter
+def die(alpha=0.05) -> None:  # alpha är amplituden för hur mycket d_prob svänger mellan det stabila d_prob = r_prob / (1 + r_prob)
     global white_counter
     global black_counter
     i = 0
@@ -111,7 +108,6 @@ def die(alpha=0.05):  # alpha är amplituden för hur mycket d_prob svänger mel
             white_counter -= bool(peppers[i].color)
             black_counter -= not bool(peppers[i].color)
             peppers.pop(i)
-            death_counter += 1
             continue
         i += 1
 
@@ -152,8 +148,6 @@ while run:
         reproduce(r_prob)
     die()
 
-    display.blit(font.render(f'Reproduce counter: {reproduce_counter}', True, v_red), (130, 0))
-    display.blit(font.render(f'Death counter: {death_counter}', True, v_red), (600, 0))
     display.blit(font.render(f'Average d_prob: {statistics.mean(average):.2%}', True, v_red), (600, 40))
     display.blit(font.render(f'Population size: {len(peppers)}', True, v_red), (130, 40))
     display.blit(font.render(f'White population size: {white_counter}', True, v_red), (130, 80))
